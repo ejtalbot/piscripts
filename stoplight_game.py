ab2c282d955658850fc98fc3b5461c565ee474a2
ejtalbot <erikjamestalbot@gmail.com>
2021-02-25 22:25:24 -0600

    change step

--- stoplight_game.py
+++ stoplight_game.py
@@ -66,7 +66,7 @@ def main():
     for i in range (blink_count):
         for pin, light in board.lights.items():
             if pin == 7:
-                light.blink(.1*(blink_count - i) + .05)
+                light.blink(.05*(blink_count - i) + .05)
     GPIO.cleanup()
 
 if __name__=="__main__":
