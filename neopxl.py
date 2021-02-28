e7bcdc660cede6c1da8080bc97388d43260f0552
ejtalbot <erikjamestalbot@gmail.com>
2021-02-28 16:36:44 -0600

    neopixel loop back

--- neopxl.py
+++ neopxl.py
@@ -2,8 +2,19 @@ import board
 import neopixel
 import time
 pixels = neopixel.NeoPixel(board.D18, 30)
+max_pixel = 10
 
-for pix_num in range(10):
+for pix_num in range(max_pixel):
+	pixels[pix_num] = (255, 0, 0)
+	time.sleep(.25)
+	pixels[pix_num] = (0, 255, 0)
+	time.sleep(.25)
+	pixels[pix_num] = (0, 0, 255)
+	time.sleep(.25)
+	pixels[pix_num] = (0, 0, 0)
+	time.sleep(.25)
+
+for pix_num in reversed(max_pixel):
 	pixels[pix_num] = (255, 0, 0)
 	time.sleep(.25)
 	pixels[pix_num] = (0, 255, 0)
