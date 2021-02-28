16480b802d74dfb43ca352e73dce2c2d37aeb5d6
ejtalbot <erikjamestalbot@gmail.com>
2021-02-28 16:52:30 -0600

    random colors

--- neopxl.py
+++ neopxl.py
@@ -1,25 +1,20 @@
 import board
 import neopixel
+import random
 import time
 pixels = neopixel.NeoPixel(board.D18, 30)
 max_pixel = 10
+blinks = 5
+
+def rand_rbg():
+	return tuple(
+		random.randint(0, 255),
+		random.randint(0, 255),
+		random.randint(0, 255)
+	)
 
 for pix_num in range(max_pixel):
-	pixels[pix_num] = (255, 0, 0)
-	time.sleep(.25)
-	pixels[pix_num] = (0, 255, 0)
-	time.sleep(.25)
-	pixels[pix_num] = (0, 0, 255)
-	time.sleep(.25)
+	for blink in range(blinks):
+		pixels[pix_num] = rand_rbg()
+		time.sleep(.25)
 	pixels[pix_num] = (0, 0, 0)
-	time.sleep(.25)
-
-for pix_num in reversed(range(max_pixel)):
-	pixels[pix_num] = (255, 0, 0)
-	time.sleep(.25)
-	pixels[pix_num] = (0, 255, 0)
-	time.sleep(.25)
-	pixels[pix_num] = (0, 0, 255)
-	time.sleep(.25)
-	pixels[pix_num] = (0, 0, 0)
-	time.sleep(.25)
