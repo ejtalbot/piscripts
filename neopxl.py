cc62e75fa6194d87c4da5fce2e3fc6b4ec6fee74
ejtalbot <erikjamestalbot@gmail.com>
2021-02-28 16:33:45 -0600

    neopixel example

---
+++ neopxl.py
@@ -0,0 +1,14 @@
+import board
+import neopixel
+import time
+pixels = neopixel.NeoPixel(board.D18, 30)
+
+for pix_num in range(10):
+	pixels[pix_num] = (255, 0, 0)
+	time.sleep(.25)
+	pixels[pix_num] = (0, 255, 0)
+	time.sleep(.25)
+	pixels[pix_num] = (0, 0, 255)
+	time.sleep(.25)
+	pixels[pix_num] = (0, 0, 0)
+	time.sleep(.25)
