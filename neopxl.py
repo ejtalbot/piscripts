ec033f33d439105776ac871f89033f390cdaff5e
ejtalbot <erikjamestalbot@gmail.com>
2021-02-28 17:07:04 -0600

    max 29

--- neopxl.py
+++ neopxl.py
@@ -3,13 +3,13 @@ import neopixel
 import random
 import time
 pixels = neopixel.NeoPixel(board.D18, 30)
-max_pixel = 60
+max_pixel = 29
 blinks = 5
 
 def rand_rbg():
-	red = random.randint(0, 254)
-	green = random.randint(0, 254)
-	blue = random.randint(0, 254)
+	red = random.randint(0, 255)
+	green = random.randint(0, 255)
+	blue = random.randint(0, 255)
 	return tuple((red, green, blue))
 
 for pix_num in range(max_pixel):
