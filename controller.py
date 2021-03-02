019294f8a9592840cfff4282a25db13d6bce0e62
ejtalbot <erikjamestalbot@gmail.com>
2021-03-01 22:32:32 -0600

    dont auto show

--- controller.py
+++ controller.py
@@ -15,7 +15,7 @@ class Board:
 	def __init__(self, gpio: str = "D18", count: int = 60):
 		self.gpio = gpio
 		self.count = count
-		self.pixels = neopixel.NeoPixel(board.__getattribute__(gpio), self.count)
+		self.pixels = neopixel.NeoPixel(board.__getattribute__(gpio), self.count, brightness=.5, auto_write=False)
 
 	def set_pixel_color(self, pixel_number: int, red: int, green: int, blue: int):
 		if any(not(0<=color<=255) for color in {red, green, blue}):
@@ -29,10 +29,12 @@ class Board:
 	def turn_off_all_pixels(self):
 		for pixel_number in range(self.count):
 			self.turn_off_pixel(pixel_number)
+		self.pixels.show()
 
 	def cycle(self, red: int, green: int, blue: int):
 		for pixel_number in range(self.count):
 			self.set_pixel_color(pixel_number, red, green, blue)
+			self.pixels.show()
 			time.sleep(.1)
 
 board = Board()
