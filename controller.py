7fce26a10312bbe9e40476f2f0e355c8ad859f05
ejtalbot <erikjamestalbot@gmail.com>
2021-03-02 21:52:05 -0600

    fading example

--- controller.py
+++ controller.py
@@ -44,13 +44,86 @@ class Board:
 		self.pixels.show()
 		time.sleep(.5)
 
+	def color_fade(self, pixel_number: int, rgb: int):
+		# get the current color of the pixel
+		if rgb not in range(3):
+			print(f"invalid {rgb}")
+		else:
+			current_pixel = self.pixels[pixel_number]
+			red = current_pixel[0]
+			green = current_pixel[1]
+			blue = current_pixel[2]
+			if rgb == 0:
+				red = max(0, current_pixel[0]-10)
+			elif: rgb == 1:
+				green = max(0, current_pixel[0]-10)
+			else:
+				blue = max(0, current_pixel[0]-10)
+			self.set_pixel_color(pixel_number, red, green, blue)
+
+	def primary_color_fade(self, pixel_number: int, rgb: int):
+		if rgb not in range(3):
+			print(f"invalid {rgb}")
+		else:
+			current_pixel = self.pixels[pixel_number]
+			red = current_pixel[0]
+			green = current_pixel[1]
+			blue = current_pixel[2]
+			if rgb == 0:
+				red = max(0, current_pixel[0]-10)
+			elif: rgb == 1:
+				green = max(0, current_pixel[0]-10)
+			else:
+				blue = max(0, current_pixel[0]-10)
+			self.set_pixel_color(pixel_number, red, green, blue)
+
+	def fade_all_primary_to_black(self, rgb: int):
+		while any(
+			pixel for pixel in self.pixels[pixel_number] if pixel[rgb] > 0
+		):
+			for pixel_number in range(self.count):
+				self.primary_color_fade(pixel_number, rgb)
+			self.pixels.show()
+			time.sleep(.1)
+
+	def primary_color_increase(self, pixel_number: int, rgb: int):
+		if rgb not in range(3):
+			print(f"invalid {rgb}")
+		else:
+			current_pixel = self.pixels[pixel_number]
+			red = current_pixel[0]
+			green = current_pixel[1]
+			blue = current_pixel[2]
+			if rgb == 0:
+				red = min(0, current_pixel[0]+10)
+			elif: rgb == 1:
+				green = min(0, current_pixel[0]+10)
+			else:
+				blue = min(0, current_pixel[0]+10)
+			self.set_pixel_color(pixel_number, red, green, blue)
+
+	def increase_all_primary(self, rgb: int):
+		while any(
+			pixel for pixel in self.pixels[pixel_number] if pixel[rgb] < 255
+		):
+			for pixel_number in range(self.count):
+				self.primary_color_increase(pixel_number, rgb)
+			self.pixels.show()
+			time.sleep(.1)
+
 
 board = Board()
-board.cycle(255, 0, 0)
-board.cycle(0, 255, 0)
-board.cycle(0, 0, 255)
-for i in range(10):
-	board.offset_light(3, i, 255, 0, 0)
-	board.offset_light(3, i+1, 0, 255, 0)
-	board.offset_light(3, i+2, 0, 0, 255)
+board.increase_all_primary(0)
+board.increase_all_primary(1)
+board.increase_all_primary(2)
+board.fade_all_primary_to_black(0)
+board.fade_all_primary_to_black(1)
+board.fade_all_primary_to_black(2)
+#board.cycle(255, 0, 0)
+#board.cycle(0, 255, 0)
+#board.cycle(0, 0, 255)
+#for i in range(10):
+#	board.offset_light(3, i, 255, 0, 0)
+#	board.offset_light(3, i+1, 0, 255, 0)
+#	board.offset_light(3, i+2, 0, 0, 255)
 board.turn_off_all_pixels()
