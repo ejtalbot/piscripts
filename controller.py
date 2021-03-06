98b68fb1d37a3772c431ecaf89ea5469ca8423f9
ejtalbot <erikjamestalbot@gmail.com>
2021-03-05 20:34:28 -0600

    test getting

--- controller.py
+++ controller.py
@@ -107,6 +107,7 @@ class Board:
 			for pixel_number in reversed(range(1, self.count)):
 				# get the previous pixel = self.pixels[pixel_number - 1]
 				red, green, blue = self.get_pixel_rgb(pixel_number)
+				print(red)
 				self.set_pixel_color(pixel_number -1, red, green, blue)
 			self.set_pixel_color(0, color[0], color[1], color[2])
 				# usee conversion
