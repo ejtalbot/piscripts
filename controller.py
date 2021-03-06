60fa94b558e1efdadceb91886fb99464eb6a011f
ejtalbot <erikjamestalbot@gmail.com>
2021-03-05 22:01:25 -0600

    subset wheel

--- controller.py
+++ controller.py
@@ -110,7 +110,7 @@ class Board:
 			time.sleep(.1)
 
 	def subset_color_wheel(self, colors: List[Tuple[str, str, str]]):
-		loop_count = 10
+		loop_count = 100
 		for i in range(loop_count):
 			for pixel_number in range(self.count):
 				color_position = (pixel_number + i) % len(colors)
