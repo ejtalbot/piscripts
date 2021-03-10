59cb734e2d5c5b6e331aab021bf46ca54edee928
ejtalbot <erikjamestalbot@gmail.com>
2021-03-10 00:47:53 -0600

    turn all background at once

--- controller.py
+++ controller.py
@@ -154,7 +154,7 @@ class Board:
 		end = len(pattern) -1
 		for background_rgb in pattern_base:
 			for i in range(crawl_length):
-				board.turn_off_all_pixels()
+				self.turn_off_all_pixels()
 				start = (start + 1) % self.count
 				end = ((end + 1) % self.count)
 				for snake_position, pixel in enumerate(range(start, start + len(pattern))):
