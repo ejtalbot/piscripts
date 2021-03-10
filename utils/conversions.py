cf9c9c2d1e93c9a4a1fc5441d2a67ec591d05422
ejtalbot <erikjamestalbot@gmail.com>
2021-03-09 23:03:46 -0600

    mv import

--- utils/conversions.py
+++ utils/conversions.py
@@ -1,7 +1,7 @@
 from typing import List, Tuple
 import os
 
-from csv_handler import read_to_color_name_dict
+from .csv_handler import read_to_color_name_dict
 
 def rgb_tuple_split(rgb_tuple: Tuple[str, str, str]):
 	return int(rgb_tuple[0]), int(rgb_tuple[1]), int(rgb_tuple[2])
