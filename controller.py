4e79bb2d7fd1646370cb5478c4b5ac9e1f9956ff
ejtalbot <erikjamestalbot@gmail.com>
2021-03-10 23:40:10 -0600

    5 x snake

--- controller.py
+++ controller.py
@@ -155,7 +155,7 @@ class Board:
 			self.set_range_of_pixels(snake.start, snake.start + len(snake.pattern), rgb_tuple_split(background_rgb), inside = False)
 			for i in range(crawl_length):
 				background_red, background_green, background_blue = rgb_tuple_split(background_rgb)
-				self.set_pixel_color(snake.start, background_red, background_green, background_blue)
+				#self.set_pixel_color(snake.start, background_red, background_green, background_blue)
 				snake.move(1)
 				snake.iteration(self.move_pattern)
 				self.pixels.show()
