437a5a46cc6b0527da4a2c304d8799c6bfb21604
ejtalbot <erikjamestalbot@gmail.com>
2021-03-13 17:50:40 -0600

    async sleep

--- controller.py
+++ controller.py
@@ -177,7 +177,7 @@ class Board:
 				snake.move(1)
 				snake.iteration(self.move_pattern)
 				self.pixels.show()
-				time.sleep(.1)
+				asyncio.sleep(.1)
 		self.turn_off_all_pixels()
 
 	def light_all_off_pixels(self, rgb: Tuple[int, int, int] = (255, 255, 255)):
