cc0d167c7a2c150bb3463195d7bf7aabd53353d5
ejtalbot <erikjamestalbot@gmail.com>
2021-03-16 22:29:08 -0500

    rm prints

--- controller.py
+++ controller.py
@@ -152,11 +152,9 @@ class Board:
 
 	#@interrupt
 	async def subset_color_wheel(self):
-		print(self.active_snake.pattern)
 		for pixel_number in range(self.count):
 			color_position = (pixel_number + self.active_snake.counter) % len(self.active_snake.pattern)
 			red, green, blue = rgb_tuple_split(self.active_snake.pattern[color_position])
-			print(pixel_number, red, blue, green)
 			self.set_pixel_color(pixel_number, red, green, blue)
 		self.pixels.show()
 		self.active_snake.counter += 1
