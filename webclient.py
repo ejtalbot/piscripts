52e21775a887ce853c5df471f609f2311247dbad
ejtalbot <erikjamestalbot@gmail.com>
2021-03-21 21:41:59 -0500

    webclient

---
+++ webclient.py
@@ -0,0 +1,18 @@
+import asyncio
+
+import websockets
+
+
+async def send_message():
+    uri = "ws://192.168.0.126:8765"
+    async with websockets.connect(uri, ping_interval=None) as websocket:
+        command = input("Enter input")
+
+        await websocket.send(command)
+        print(f"> {command}")
+
+        response = await websocket.recv()
+        print(f"< {response}")
+
+
+asyncio.get_event_loop().run_until_complete(send_message())
