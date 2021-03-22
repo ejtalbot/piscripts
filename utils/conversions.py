b543f7adcf9f9236c129dc88f9670488a531e560
ejtalbot <erikjamestalbot@gmail.com>
2021-03-21 19:11:36 -0500

    flake utils

--- utils/conversions.py
+++ utils/conversions.py
@@ -3,36 +3,39 @@ import os
 
 from .csv_handler import read_to_color_name_dict
 
+
 def rgb_tuple_split(rgb_tuple: Tuple[str, str, str]):
-	return int(rgb_tuple[0]), int(rgb_tuple[1]), int(rgb_tuple[2])
+    return int(rgb_tuple[0]), int(rgb_tuple[1]), int(rgb_tuple[2])
+
 
 def create_rgb_tuple(red: int, green: int, blue: int):
-	return tuple(red, green, blue)
+    return tuple(red, green, blue)
+
 
 def lengthen_sequence(sequence: List[str], multiplier: int):
-	return [
-		sub_item for sub_items in [[item]*multiplier for item in sequence] for sub_item in sub_items
-	]
+    return [
+        sub_item
+        for sub_items in [[item] * multiplier for item in sequence]
+        for sub_item in sub_items
+    ]
 
-def create_color_pattern_by_name(color_names: List[str]) -> List[Tuple[str,str,str]]:
-	"""Gte a color pattern based on names passed in list"""
-	pattern_rgb_colors = list()
-	color_name_dict = read_to_color_name_dict(
-		os.path.join(
-			os.path.abspath(
-				os.path.dirname(
-					'resources/colors.csv'
-				)
-			),
-			"colors.csv"
-		)
-	)
-	for color in color_names:
-		pattern_rgb_colors.append(color_name_dict.get(color, (0,0,0)))
-	return pattern_rgb_colors
+
+def create_color_pattern_by_name(color_names: List[str]) -> List[Tuple[str, str, str]]:
+    """Gte a color pattern based on names passed in list"""
+    pattern_rgb_colors = list()
+    color_name_dict = read_to_color_name_dict(
+        os.path.join(
+            os.path.abspath(os.path.dirname("resources/colors.csv")), "colors.csv"
+        )
+    )
+    for color in color_names:
+        pattern_rgb_colors.append(color_name_dict.get(color, (0, 0, 0)))
+    return pattern_rgb_colors
+
 
 def convert_strings_in_tuple_to_ints(rgb_tuple: Tuple[str, str, str]):
-	return (int(rgb_tuple[0]), int(rgb_tuple[1]), int(rgb_tuple[2]))
+    return (int(rgb_tuple[0]), int(rgb_tuple[1]), int(rgb_tuple[2]))
+
 
 def modulo_position_in_count(position: int, count: int):
-	return position if position < count else (pixel + 1) % count
+    return position if position < count else (position + 1) % count
--- utils/decorators.py
+++ utils/decorators.py
@@ -1,15 +1,16 @@
-import asyncio
 from functools import wraps
 
+
 def interrupt(func):
-	@wraps(func)
-	async def wrapper(*args, **kwargs):
-		board_object = args[0]
-		try:
-			while not board_object.off_switch:
-				await func(*args, **kwargs)
-			print(f"broken out {board_object.off_switch}")
-			board_object.turn_off_all_pixels()
-		except KeyboardInterrupt:
-			print("Process interrupted")
-	return wrapper
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        board_object = args[0]
+        try:
+            while not board_object.off_switch:
+                await func(*args, **kwargs)
+            print(f"broken out {board_object.off_switch}")
+            board_object.turn_off_all_pixels()
+        except KeyboardInterrupt:
+            print("Process interrupted")
+
+    return wrapper
